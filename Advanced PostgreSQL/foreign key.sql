2721817ffea801691e6e02f7b209e2bdd059b79c
jabradf <146892014+jabradf@users.noreply.github.com>
2023-12-11 19:39:53 +1000

    Update foreign key.sql

--- Advanced PostgreSQL/foreign key.sql
+++ Advanced PostgreSQL/foreign key.sql
@@ -1,5 +1,5 @@
 ALTER TABLE talks
 ADD FOREIGN KEY (speaker_id)
-REFERENCES speakers (id);
+REFERENCES speakers (id);  -- this is the foreign key reference
 
 INSERT INTO talks VALUES (21, 'Data Warehousing Best Practises', 101, 30, '2020-08-15 18:00');
